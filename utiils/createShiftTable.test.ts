8147d9c85b02eb65791a49cbd9829b4b1a20f705
yamakeeeeeeeeen <yamakeeeeeeeeen@gmail.com>
2023-08-09 00:01:13 +0900

    refactor: 共通処理を関数に切り出す

--- utiils/createShiftTable.test.ts
+++ utiils/createShiftTable.test.ts
@@ -3,11 +3,32 @@ import * as path from "path";
 import { createShiftTable } from "./createShiftTable";
 
 const TEST_DIR = "./test";
+const outputFilePath = `${TEST_DIR}/output/shift.csv`;
+
+const createAndCheckShiftTable = async (
+  version: string,
+  expectedCsv: string,
+) => {
+  const paths = {
+    caregiver: `${TEST_DIR}/csv/${version}/caregiver.csv`,
+    user: `${TEST_DIR}/csv/${version}/user.csv`,
+    output: outputFilePath,
+  };
+
+  // 出力ディレクトリを作成
+  const outputDir = path.dirname(paths.output);
+  await fs.mkdir(outputDir, { recursive: true });
+
+  await createShiftTable(paths);
+
+  // 出力ファイルを読み込み
+  const outputCsv = await fs.readFile(paths.output, "utf8");
+
+  // 出力が期待するCSVと一致しているかを確認
+  expect(outputCsv).toBe(expectedCsv);
+};
 
 describe("createShiftTable", () => {
-  const outputFilePath = `${TEST_DIR}/output/shift.csv`;
-
-  // テスト終了後に出力ファイルを削除
   afterAll(async () => {
     try {
       await fs.unlink(outputFilePath);
@@ -20,72 +41,18 @@ describe("createShiftTable", () => {
 
   describe("ユーザーの利用開始時間を元に結果がソートされていること", () => {
     it("v1: 介護士とユーザーのCSVデータからシフトテーブルが適切に作成されること", async () => {
-      const paths = {
-        caregiver: `${TEST_DIR}/csv/v1/caregiver.csv`,
-        user: `${TEST_DIR}/csv/v1/user.csv`,
-        output: outputFilePath,
-      };
-
-      // 出力ディレクトリを作成
-      const outputDir = path.dirname(paths.output);
-      await fs.mkdir(outputDir, { recursive: true });
-
-      await createShiftTable(paths);
-
-      // 出力ファイルを読み込み
-      const outputCsv = await fs.readFile(paths.output, "utf8");
-
-      // 期待するCSV文字列（末尾の改行を削除）
-      const expectedCsv = `"caregiver","user","start_time","end_time"\n"Alice","User3","09:00","17:00"\n"Bob","User1","10:00","15:00"\n"Charlie","User2","14:00","20:00"`;
-
-      // 出力が期待するCSVと一致しているかを確認
-      expect(outputCsv).toBe(expectedCsv);
+      const expected = `"caregiver","user","start_time","end_time"\n"Alice","User3","09:00","17:00"\n"Bob","User1","10:00","15:00"\n"Charlie","User2","14:00","20:00"`;
+      await createAndCheckShiftTable("v1", expected);
     });
 
     it("v2: 全ての介護士とユーザーの組み合わせを確認し、なるべくあまりが出ないようにシフトテーブルが作成されること", async () => {
-      const paths = {
-        caregiver: `${TEST_DIR}/csv/v2/caregiver.csv`,
-        user: `${TEST_DIR}/csv/v2/user.csv`,
-        output: outputFilePath,
-      };
-
-      // 出力ディレクトリを作成
-      const outputDir = path.dirname(paths.output);
-      await fs.mkdir(outputDir, { recursive: true });
-
-      await createShiftTable(paths);
-
-      // 出力ファイルを読み込み
-      const outputCsv = await fs.readFile(paths.output, "utf8");
-
-      // 期待するCSV文字列（末尾の改行を削除）
-      const expectedCsv = `"caregiver","user","start_time","end_time"\n"Alice","User3","09:00","17:00"\n"Bob","User1","10:00","15:00"\n"Charlie","User2","14:00","20:00"\n"Daniel","User5","11:00","17:00"\n"Eric","User4","12:00","18:00"`;
-
-      // 出力が期待するCSVと一致しているかを確認
-      expect(outputCsv).toBe(expectedCsv);
+      const expected = `"caregiver","user","start_time","end_time"\n"Alice","User3","09:00","17:00"\n"Bob","User1","10:00","15:00"\n"Charlie","User2","14:00","20:00"\n"Daniel","User5","11:00","17:00"\n"Eric","User4","12:00","18:00"`;
+      await createAndCheckShiftTable("v2", expected);
     });
 
     it("v3: 全ての介護士とユーザーの相性を考慮したシフトテーブルが作成されること（相性が良い組み合わせを作る）", async () => {
-      const paths = {
-        caregiver: `${TEST_DIR}/csv/v3/caregiver.csv`,
-        user: `${TEST_DIR}/csv/v3/user.csv`,
-        output: outputFilePath,
-      };
-
-      // 出力ディレクトリを作成
-      const outputDir = path.dirname(paths.output);
-      await fs.mkdir(outputDir, { recursive: true });
-
-      await createShiftTable(paths);
-
-      // 出力ファイルを読み込み
-      const outputCsv = await fs.readFile(paths.output, "utf8");
-
-      // 期待するCSV文字列（末尾の改行を削除）
-      const expectedCsv = `"caregiver","user","start_time","end_time"\n"Alice","User1","10:00","15:00"\n"Charlie","User2","14:00","20:00"\n"Daniel","User5","11:00","17:00"\n"Eric","User4","12:00","18:00"\n"Franks","User3","09:00","17:00"`;
-
-      // 出力が期待するCSVと一致しているかを確認
-      expect(outputCsv).toBe(expectedCsv);
+      const expected = `"caregiver","user","start_time","end_time"\n"Alice","User1","10:00","15:00"\n"Charlie","User2","14:00","20:00"\n"Daniel","User5","11:00","17:00"\n"Eric","User4","12:00","18:00"\n"Franks","User3","09:00","17:00"`;
+      await createAndCheckShiftTable("v3", expected);
     });
   });
 });
